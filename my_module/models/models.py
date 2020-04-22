dec253cd659767e75a6133158653acc1e70820a1
John Cottam <jcc@industrialbrush.com>
2020-04-22 23:41:10 +0000

    indentationgit status

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 
- class my_module(models.Model):
+class my_module(models.Model):
      _name = 'my_module.my_module'
      _description = 'my_module.my_module'
 
